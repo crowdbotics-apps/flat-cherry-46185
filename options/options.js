9aa213932598969986899f59168bed5e46cfa5c4
Crowdbotics <team@crowdbotics.com>
2024-02-09 17:51:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "flat-cherry-46185",
+  url: "https://flat-cherry-46185.botics.co",
+  api: "https://flat-cherry-46185.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
